f18fd733cd8a36991c4483236f613804aafbe66f
kaioFerreira <kaiofhs@hotmail.com>
2021-03-15 12:51:00 -0300

    Fix: Error ViewSet

--- api/urls.py
+++ api/urls.py
@@ -5,13 +5,14 @@ from .views import AcompanhamentoViewSet
 from rest_framework import routers
 
 
-router = routers.DefaultRouter()
-router.register(r'acompanhamento100', AcompanhamentoViewSet, basename='acompanhamento')
+# router = routers.DefaultRouter()
+# router.register(r'acompanhamento100', AcompanhamentoViewSet.get, basename='acompanhamento')
 
 
 urlpatterns = [
     # url(r'^', include(router.urls), name='acompanhamento'),
-    path('', include(router.urls) , name='acompanhamento'),
+    path('acompanhamento100/', AcompanhamentoViewSet.get , name='acompanhamento-get'),
+    path('acompanhamentopost/', AcompanhamentoViewSet.post , name='acompanhamento-set'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from core.models import Acompanhamento
 from core.serializers import AcompanhamentoSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 class AcompanhamentoViewSet(viewsets.ModelViewSet):
-	queryset = Acompanhamento.objects.all()
 	serializer_class = AcompanhamentoSerializer
+	@api_view(['GET'])
+	def get(request):
+		queryset = Acompanhamento.objects.all()
+		serializer = AcompanhamentoSerializer(queryset, many=True)
+		return Response(serializer.data)
+
+	@api_view(['POST'])
+	def post(request):
+		serializer = AcompanhamentoSerializer(data=request.data)
+
+		if serializer.is_valid():
+			serializer.save()
+		
+		return Response(serializer.data)
