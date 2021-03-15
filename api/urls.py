14757fce26cc5e4d364ac1cccb5e8f53a40900e8
kaioFerreira <kaiofhs@hotmail.com>
2021-03-15 12:17:32 -0300

    Fix: Error ViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from .views import Acompanhamento
+from .views import AcompanhamentoViewSet
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
-router.register(r'acompanhamento100', Acompanhamento, basename='acompanhamento')
+router.register(r'acompanhamento100', AcompanhamentoViewSet, basename='acompanhamento')
 
 
 urlpatterns = [
     # url(r'^', include(router.urls), name='acompanhamento'),
-    path('', include(router.urls), name='acompanhamento'),
+    path('', include(router.urls) , name='acompanhamento'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
